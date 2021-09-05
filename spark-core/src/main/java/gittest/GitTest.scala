b8890dc7fc555a4623956087991f3f6bf7e0c49e
yujiawang <yujiawang@123.com>
2021-09-05 19:46:57 +0800

    hot-fix commit2

--- spark-core/src/main/java/gittest/GitTest.scala
+++ spark-core/src/main/java/gittest/GitTest.scala
@@ -7,11 +7,5 @@ package gittest
 class GitTest {
   def main(args: Array[String]): Unit = {
     println("hello")
-    println("hello2")
-    println("hello2")
-    println("hello git")
-    println("hello git")
-    println("hello hot-fix")
-    println("hello hot-fix")
   }
 }
